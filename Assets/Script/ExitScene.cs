225d26a83275bdf09a72a1cba2411d80a365a831
LittleUser <anjunh981@gmail.com>
2022-04-25 16:57:54 +0900

    git init

---
+++ Assets/Script/ExitScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitScene : MonoBehaviour
+{
+    public void GameExit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
---
+++ Assets/Script/NPC.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour,IInterAction
+{
+   public Conversation conversation;
+  public Quest[] quests;
+  //public Quest curQuest = null;
+  private void Awake() {
+    quests = GetComponentsInChildren<Quest>();
+  }
+  public bool ReAction()
+  {
+     // 1. 가능한 퀘 있으면 퀘
+     foreach(Quest quest in quests)
+     {
+       if(quest.isActive)
+       {
+         return quest.ReAction();
+       }
+     }
+     // 2. 퀘 없으면 대화
+     return  conversation.ReAction();
+  }
+}
