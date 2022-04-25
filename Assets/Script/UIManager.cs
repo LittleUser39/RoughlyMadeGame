6708b5f417c02d33eda7f3ee27d11e805f228008
LittleUser39 <anjunh981@gmail.com>
2022-04-25 20:45:27 +0900

    중간점검
    MENU UI 창 중간까지 만듦

---
+++ Assets/Script/UIManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class UIManager : MonoBehaviour
+{
+   public GameObject dialog;
+    public void SetActiveDialog(bool active)
+    {
+        dialog.SetActive(active);
+    }
+    public bool GetActiveDialog()
+    {
+        return dialog.activeSelf; 
+    }
+}
