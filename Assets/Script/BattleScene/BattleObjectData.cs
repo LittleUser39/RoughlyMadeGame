0c380687819e5901720f2b91fed4212103d3a3f5
Junsik-Yoon <epsqjaos@naver.com>
2022-04-26 21:36:25 +0900

    화요일중간머지
    배틀매니저상호작용 중간점검

--- Assets/Script/BattleScene/BattleObjectData.cs
+++ Assets/Script/BattleScene/BattleObjectData.cs
@@ -6,9 +6,11 @@ public abstract class BattleObjectData : ScriptableObject
     
     public GameObject prefab = null;
     
+    public int index;
     new public string name = "New Item";
     public int HP = 100;
-    public int agility;//턴 돌아오는 속도
+    public float agility;//턴 돌아오는 속도
+    public float DefaultAgility;//안바뀌는값;
     public int damage;//기본 데미지
 
     // 0 = 불 , 1, 물, 2, 풀
---
+++ Assets/Script/BattleScene/PlayerMoveBattle.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveBattle : MonoBehaviour
+{   
+    public bool isTurn;
+    public float moveSpeed = 1f;
+    Vector2 prevPos;
+    Vector2 dir;
+    bool isMoving=false;
+    private void FixedUpdate()
+    {
+        if(isMoving)
+        {
+            Debug.Log(dir);
+            transform.Translate(dir*moveSpeed*Time.deltaTime);
+        }
+    }
+    public void Approach(Vector2 _dir)
+    {
+        prevPos = transform.position;
+        dir = _dir;
+        isMoving=true;
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag=="Monster" && isTurn)
+        {
+            dir = (prevPos - (Vector2)transform.position).normalized;
+            StartCoroutine(StopMoving());
+        }
+    }
+    IEnumerator StopMoving()
+    {
+        yield return new WaitForSeconds(0.2f);
+        isMoving = false;
+        transform.position = prevPos;
+        BattleManager.instance.isBattlePaused = false;
+        isTurn=false;
+        StopCoroutine(StopMoving());
+    }
+}
